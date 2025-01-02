496dc3dc0b0c5dbee11d2d7ce3dbcfa6fa4292e5
Ralph <ralcober01@manhattan.edu>
2025-01-01 19:58:58 -0500

    added sound and animations

--- scripts/cars.js
+++ scripts/cars.js
@@ -6,33 +6,205 @@ class CarObject {
     this.dlc = dlc;
     this.seats = seats;
     this.image = `../images/cars/${image}.png`;
-    this.price = price;
+    this.price = price.toLocaleString();
     this.categories = categories;
   }
 }
 
-const car1 = new CarObject(
-  'PEGASSI TORERO XO',
-  'PART OF THE CRIMINAL ENTERPRISES',
+carList.push(new CarObject(
+  'DINKA JESTER RR WIDEBODY',
+  'PART OF AGENTS OF SABOTAGE',
   2,
-  'car-image1',
-  289000,
+  'jester',
+  2290000,
   ['FEATURED',
     'DOOR2',
-    'SPECIAL'
   ]
-)
-const car2 = new CarObject(
-  'SECOND CAR',
-  'TEST CAR DLC',
+),
+new CarObject(
+  'BRAVADO BANSHEE GTS',
+  'PART OF AGENTS OF SABOTAGE',
   2,
-  'car-image1',
-  2333000,
-  [ 'DOOR4',
-    'SPECIAL'
+  'bansheegts',
+  1989500,
+  [ 'DOOR2',
+    'FEATURED'
   ]
+),
+new CarObject(
+  'OVERFLOD PIPISTRELLO',
+  'PART OF BOTTOM DOLLAR BOUNTIES',
+  2,
+  'pipistrello',
+  2950000,
+  [ 'DOOR2',
+    'FEATURED'
+  ]
+),
+new CarObject(
+  'INVETERO CONQUETTE D1',
+  'PART OF BOTTOM DOLLAR BOUNTIES',
+  2,
+  'coquette',
+  1500000,
+  [ 'DOOR2',
+    'FEATURED'
+  ]
+),
+new CarObject(
+  'ANNIS EUROS X32',
+  'PART OF BOTTOM DOLLAR BOUNTIES',
+  2,
+  'euros',
+  1499000,
+  [ 'DOOR2',
+    'FEATURED'
+  ]
+),
+new CarObject(
+  'UBERMACT NIOBE',
+  'PART OF BOTTOM DOLLAR BOUNTIES',
+  2,
+  'niobe',
+  1880000,
+  [ 'DOOR2',
+    'FEATURED'
+  ]
+),
+new CarObject(
+  'GALLIVANTER BALLER ST-D',
+  'PART OF THE CHOP SHOP',
+  4,
+  'baller',
+  1715000,
+  [ 'DOOR4'
+  ]
+),
+new CarObject(
+  'VAPID ALEUTIAN',
+  'PART OF THE CHOP SHOP',
+  4,
+  'aleutian',
+  1835000,
+  [ 'DOOR4'
+  ]
+),
+new CarObject(
+  'OBEY OMNIS E-GT',
+  'PART OF THE CRIMINAL ENTERPRISES',
+  4,
+  'omnis',
+  1795000,
+  [ 'DOOR4'
+  ]
+),
+new CarObject(
+  'PFISTER ASTRON',
+  'PART OF THE CONTRACT',
+  4,
+  'astron',
+  1580000,
+  [ 'DOOR4'
+  ]
+),
+new CarObject(
+  'LAMPADATI CINQUEMILA',
+  'PART OF THE CONTRACT',
+  4,
+  'cinquemila',
+  1740000,
+  [ 'DOOR4'
+  ]
+),
+new CarObject(
+  'WESTERN POWERSURGE',
+  'PART OF LOS SANTOS DURG WARS',
+  1,
+  'powersurge',
+  1605000,
+  [ 'MOTORCYCLES'
+  ]
+),
+new CarObject(
+  'WESTERN REEVER',
+  'PART OF THE CONTRACT',
+  1,
+  'reever',
+  1900000,
+  [ 'MOTORCYCLES'
+  ]
+),
+new CarObject(
+  'NAGASAKI SHINOBI',
+  'PART OF THE CONTRACT',
+  1,
+  'shinobi',
+  2480500,
+  [ 'MOTORCYCLES'
+  ]
+),
+new CarObject(
+  'NAGASAKI STRYDER',
+  'PART OF THE DIAMOND CASINO HEIST',
+  2,
+  'stryd',
+  670000,
+  [ 'MOTORCYCLES'
+  ]
+),
+new CarObject(
+  'WESTERN RAMPANT ROCKET',
+  'PART OF THE DIAMOND CASINO & RESORT',
+  1,
+  'rampant',
+  925000,
+  [ 'MOTORCYCLES'
+  ]
+),
+new CarObject(
+  'NAGASAKI SHOTARO',
+  'PART OF BIKERS',
+  1,
+  'shotaro',
+  2225000,
+  [ 'MOTORCYCLES'
+  ]
+),
+new CarObject(
+  'BENEFACTOR BR8',
+  'PART OF LOS SANTOS SUMMER SPECIAL',
+  1,
+  'br8',
+  3400000,
+  [ 'SPECIAL'
+  ]
+),
+new CarObject(
+  'DECLASSE DR1',
+  'PART OF LOS SANTOS SUMMER SPECIAL',
+  1,
+  'dr1',
+  2997000,
+  [ 'SPECIAL'
+  ]
+),
+new CarObject(
+  'BENEFACTOR BR8',
+  'PART OF THE DIAMOND CASINO HEIST',
+  1,
+  'pr4',
+  3515000,
+  [ 'SPECIAL'
+  ]
+),
+new CarObject(
+  'OCELOT R88',
+  'PART OF THE DIAMOND CASINO HEIST',
+  1,
+  'r88',
+  3115000,
+  [ 'SPECIAL'
+  ]
+),
 )
  
-
-carList.push(car1);
-carList.push(car2);
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,23 @@
 import { carList } from "./cars.js";
 
+const audioHoverSelect = new Audio('./audio/phone/hover-select.mp3');
+const audioSelect = new Audio('./audio/phone/select.mp3')
+
+const allButtons = document.querySelectorAll('button');
+
+allButtons.forEach((button) => {
+  button.addEventListener('mouseenter', () => {
+    audioHoverSelect.currentTime = 0;
+    audioHoverSelect.play();
+  });
+  button.addEventListener('click', () => {
+    audioSelect.currentTime = 0;
+    audioSelect.play();
+  });
+});
+
+
+
 // Buttons Code // Make Class
 
 let carListFiltered = []
@@ -16,17 +34,38 @@ buttonFilters.forEach((button) => {
   });
 });
 
+const priceButton = document.querySelector('.js-sort-by-price-button')
+const priceButtonImage = document.querySelector('.js-sort-by-price-button-image')
+
+
+priceButton.addEventListener('click', () => {
+  if (!priceButtonImage.classList.contains('selected')){
+    priceButtonImage.classList.add('selected');
+  }
+  if (priceButtonImage.src.includes('price-arrow-down.png')) {
+    priceButtonImage.src = './images/icons/price-arrow-up.png';
+  } else {
+    priceButtonImage.src = './images/icons/price-arrow-down.png';
+  }
+});
+
 buttonFilters[0].classList.add('selected');
 render('FEATURED')
 
 
 function render(filterPick) {
+
+  // Reset the sort button
+  priceButtonImage.classList.remove('selected');
+  priceButtonImage.src = './images/icons/dash.webp'
+
+
   document.querySelector('.js-car-grid').innerHTML = '';
   carListFiltered = carList.filter((car) => car.categories.includes(filterPick));
   let html = ''; 
-  carListFiltered.forEach((car) => {
+  carListFiltered.forEach((car, index) => {
   html = `
-    <div class="car-item">
+    <div class="car-item" style="--fade-delay: ${index * 0.1}s;">
       <div class="car-dlc">
         <p class="car-dlc-text">${car.dlc}</p>
         <div class="car-seat-image-container">
@@ -42,7 +81,7 @@ function render(filterPick) {
       </div>
     </div>
   `;
-  console.log('hello');
+  console.log('hello');   
   document.querySelector('.js-car-grid').innerHTML += html;
   })
   
