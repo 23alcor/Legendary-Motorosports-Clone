f0cfc06adf7b22b3a699f0542ee2dd58c7af799e
Ralph <ralcober01@manhattan.edu>
2025-01-06 21:23:28 -0500

    dash

--- scripts/index.js
+++ scripts/index.js
@@ -32,7 +32,7 @@ buttonFilters.forEach((button) => {
   button.addEventListener('click', () => {
     // Reset sort by price
     priceButtonImage.classList.remove('selected');
-    priceButtonImage.src = '../images/icons/dash.webp'
+    priceButtonImage.src = 'images/icons/dash.webp'
 
     buttonFilters.forEach((btn) => {
       btn.classList.remove('selected');
